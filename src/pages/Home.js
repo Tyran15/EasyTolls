2be4254370e3429e5992ebbfc170a01d865724cc
Tyran15 <mh15122006@gmail.com>
2023-06-13 20:32:55 -0300

    Recuperação de modificações

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,24 @@
+import {Link} from "react-router-dom";
+import './Home.css'
+import banner from '../Imagens/Logo-site/Imagem-Home.png'
+import office from '../Imagens/Stack-imagens/Office.jpg'
+import Login from "./Login/Login"
+
+
 function Home(){
 
     return(
         <div>
-            <h1>
-                Home
-            </h1>
+            <div id='base-banner'>
+                <img src={banner} id='banner' alt='banner'/>
+                <Link to="./Login">
+                    <button id="button">Faça login</button>
+                    </Link>
+            </div>
+            <div className='base1'>
+                <img src={office} id='office' />
+                <h1>O que é a EasyTolls?</h1>
+            </div>
         </div>
     );
 
