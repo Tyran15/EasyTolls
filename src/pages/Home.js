b657cdcb53956c4481980bb441c7f1d605ede5c0
FelipeFLX <felipe.frlima@gmail.com>
2023-06-14 17:01:08 -0300

    Hello world, parte 2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import {Link} from "react-router-dom";
 import './Home.css'
 import banner from '../Imagens/Logo-site/Imagem-Home.png'
-import office from '../Imagens/Stack-imagens/Office.jpg'
+import office from '../Imagens/Gifs/Office.gif'
+import objetivo from '../Imagens/Gifs/objetivo.gif'
 import Login from "./Login/Login"
 
 
@@ -13,11 +14,21 @@ function Home(){
                 <img src={banner} id='banner' alt='banner'/>
                 <Link to="./Login">
                     <button id="button">Faça login</button>
-                    </Link>
+                </Link>
             </div>
             <div className='base1'>
                 <img src={office} id='office' />
-                <h1>O que é a EasyTolls?</h1>
+                <div className="texto">
+                    <h1 className="Titulo">O que é a EasyTools?</h1>
+                    <p>Somos uma empresa que foi criada com o objetivo de facilitar a administração dentro de instituições escolares e empresarial. Hoje estamos já com mais de 10 mil instituições no Brasil e sempre procurando nos expandir mais.</p>
+                </div>
+            </div>
+            <div className='base1'>
+                <div className="texto">
+                    <h1 className="Titulo">Qual o nosso objetivo?</h1>
+                    <p>Quando tivemos a ideia de criar essa empresa foi pensando na facilidade para que empresas e insttuições de ensino tivessem apenas um local para que por lá conseguissem administrar o que acontece dentro de sua instituição.</p>
+                </div>
+                <img src={objetivo} id='objetivo' />
             </div>
         </div>
     );
