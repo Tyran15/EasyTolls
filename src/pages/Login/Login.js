8918c4e9f3fba3ece6b0e2ae46bdf11809512afd
Tyran15 <mh15122006@gmail.com>
2023-06-15 15:18:36 -0300

    Adicionamento de estilo no login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,14 +1,22 @@
 import './Login.css'
 import Formulario from '../Formulário/Formulario';
+import login from '../Imagens/Gifs/Figura login.gif'
 
 function Login(){
 
     return(
-        <div>
-            <h1>Login</h1>
-            <input type="text" id="nome" placeholder="Digite o seu nome" name='nome'/>
-            <input type="password" id="senha" placeholder="Digite a sua senha" name='senha'/>
-            <button >Confirmar</button>
+        <div className='Login'>
+            <div id='texto-img'>
+                    <p className='texto1'><b>Faça login</b></p>
+                    <p className='texto1'><b>E entre para o nosso time</b></p>
+                    <img src={login}/>
+                </div>
+            <div id='base-input'>
+                <h1 id='tiulo'>Login</h1>
+                <input className='campo-texto' type='email' placeholder='Digite seu email'></input>
+                <input className='campo-texto' type='password' placeholder='Digite sua senha'></input>
+                <button id='confirmar'>Confimar</button>
+            </div>
         </div>
     );
 
