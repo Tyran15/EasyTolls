3fb31e2375fd5288d3c4307996bd9439aa0f048c
RyanSantos26 <mh15122006@gmail.com>
2023-06-20 15:17:36 -0300

    Remoção sistema de login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,39 +1,9 @@
 import './Login.css'
 import Formulario from '../Formulário/Formulario';
-import login from '../Imagens/Gifs/Privacy-policy-unscreen.gif'
+import login from '../Imagens/Gifs/Figura login.gif'
 import { useState } from 'react';
 
-function initialState(){
-    return {user: '', password: ''};
-}
-
-function login({ user, password}){
-    if (user === 'admin' &&  password === 'admin'){
-        return { token: '1234'}
-    }
-    return {error: "Usúario ou senha inválida"}
-}
-
 function Login(){
-
-    const UserLogin = () => {
-        const [values, setValues] = useState(initialState);
-    }
-
-    function onChange(event){
-        const {value, name} = event.target;
-        
-        setValues({
-            ...values,
-            [name]: value,
-        });
-    }
-
-    function onSubmit(event){
-        event.preventDefault();
-    }
-
-    //* 16:59 https://www.youtube.com/watch?v=FVxdFxxkdDI
 
     return(
         <div className='Login'>
@@ -44,8 +14,8 @@ function Login(){
                 </div>
             <div id='base-input'>
                 <h1 id='tiulo'>Login</h1>
-                <input className='campo-texto' type='email' placeholder='Digite seu email' onChange={onChange}></input>
-                <input className='campo-texto' type='password' placeholder='Digite sua senha' onChange={onChange} value={values.password}></input>
+                <input className='campo-texto' type='email' placeholder='Digite seu email' ></input>
+                <input className='campo-texto' type='password' placeholder='Digite sua senha'></input>
                 <button id='confirmar'>Confimar</button>
             </div>
         </div>
